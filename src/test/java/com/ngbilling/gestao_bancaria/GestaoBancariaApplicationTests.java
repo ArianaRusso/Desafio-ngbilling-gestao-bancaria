43d41174bb18d06222d86e99f0527369e06ab9e8
Ariana Russo <ariana.russo@gft.com>
2025-08-05 18:47:28 -0300

    Primeiro commit

---
+++ src/test/java/com/ngbilling/gestao_bancaria/GestaoBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngbilling.gestao_bancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoBancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
